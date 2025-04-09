320d83387505b6b938d6e470b78c2b6b421f04d0
joao-gfs <joaoguilhermeferreira19@gmail.com>
2025-04-09 18:04:53 -0300

    insercao dos order details

--- backends/driver_injection/controller/order_controller.py
+++ backends/driver_injection/controller/order_controller.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('../')
 import dao.order_dao as dao
-from model.order_model import Order
+from model.order_model import Order, Order_Details
 
-def inserir_pedido(orderid, customerid, employeeid):
+def inserir_pedido(orderid, customerid, employeeid, produtos=[]):
     con_db = dao.conectar_bd()
     sessao = con_db.cursor()
 
@@ -17,18 +17,38 @@ def inserir_pedido(orderid, customerid, employeeid):
     order_existe = sessao.fetchall()
 
     if cliente_existe == []:
-        return f"Cliente {customerid} não existe"
+        return False, f"Cliente {customerid} não existe"
     if func_existe == []:
-        return f"Funcionário {employeeid} não existe"
+        return False, f"Funcionário {employeeid} não existe"
     if order_existe != []:
-        return f"Pedido com id {orderid} já existe"
+        return False, f"Pedido com id {orderid} já existe"
     
     pedido = Order(orderid, customerid, employeeid)
+
+    for produto in produtos:
+        sessao.execute(f"SELECT productid FROM northwind.products WHERE productid = '{produto['productid']}'")
+        prod_existe = sessao.fetchall()
+        if prod_existe == []:
+            return False, f"Produto {produto['productid']} não existe"
+
+    for produto in produtos:
+        details = Order_Details(pedido.orderid, produto['productid'], produto['unitprice'], produto['quantity'])
+        pedido.inserir_produto(details)
+
     res = dao.inserir_pedido(pedido)
     sessao.close()
     con_db.close()
     return res
 
 if __name__ == '__main__':
-    res = inserir_pedido(21, 'ALFKI', 5)
-    print(res)
+    produtos = [
+        {"productid": 1, "unitprice": 14.00, "quantity": 10},
+        {"productid": 2, "unitprice": 18.00, "quantity": 5}
+    ]
+
+    sucesso, mensagem = inserir_pedido(1, 'ALFKI', 5, produtos)
+
+    if sucesso:
+        print("Sucesso:", mensagem)
+    else:
+        print("Erro:", mensagem)
--- backends/driver_injection/dao/order_dao.py
+++ backends/driver_injection/dao/order_dao.py
@@ -15,16 +15,30 @@ def conectar_bd():
 
 def inserir_pedido(pedido):
     con = conectar_bd()
+    if not con:
+        return False, "Erro de conexão com banco de dados"
     sessao = con.cursor()
 
     try:
         sessao.execute(
             f"INSERT INTO northwind.orders (orderid, customerid, employeeid) VALUES ({pedido.orderid}, '{pedido.customerid}', {pedido.employeeid});"
         )
+
+        for produto in pedido.products:
+            inserir_order_detail(sessao, produto)
+
         con.commit()
-        return "Pedido inserido com sucesso!"
+        return True, "Pedido inserido com sucesso!"
     except Exception as e:
-        return f"Erro ao inserir pedido: \n{e}"
+        return False, f"Erro ao inserir pedido: \n{e}"
     finally:
         sessao.close()
         con.close()
+
+def inserir_order_detail(sessao, order_detail):
+    try:
+        sessao.execute(
+            f"INSERT INTO northwind.order_details (orderid, productid, unitprice, quantity) VALUES ({order_detail.orderid}, {order_detail.productid}, {order_detail.unitprice}, {order_detail.quantity})"
+        )
+    except Exception as e:
+        raise Exception(f"Erro ao inserir pedido:\n {e}")
